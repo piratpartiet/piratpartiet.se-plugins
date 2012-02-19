15b0e96ab15fe35e2a97abeb9e570bc6779cc4e8
Rickard Andersson <rickard@montania.se>
2012-02-19 16:46:36 +0100

    Ny plugin för att hantera startsidan via RSS

---
+++ pp-ettan/pages/options_page_ettan.php
@@ -0,0 +1,99 @@
+<?php
+if (!current_user_can('manage_options'))  {
+	wp_die( __('You do not have sufficient permissions to access this page.') );
+}
+?>
+
+<div class="wrap">
+	<h2>Underbloggar som ska visas på ettan</h2>
+
+	<?php if ( count($errors) > 0) : ?>
+		<?php foreach ( $errors as $error ) : ?>
+			<div class="error below-h2"><?php echo $error ?></div>
+		<?php endforeach ?>
+	<?php endif ?>
+
+	<?php if ( count($sites) > 0 ) : ?>
+
+	<h3>Befintliga sajter</h3>
+
+	<table class="widefat">
+		<thead>
+			<tr>
+				<th>Namn</th>
+				<th>Adress</th>
+				<th>Status</th>
+				<th>Senaste hämtning</th>
+				<th>Senast uppdaterad</th>
+				<th>Antal inlägg</th>
+				<th>Radera</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ( $sites as $key => $site ) : ?>
+			<tr>
+				<td><?php echo $site->name ?></td>
+				<td>
+					<a href="<?php echo $site->url ?>" target="_blank"><?php echo $site->url ?></a>
+				</td>
+				<td><?php echo $site->status ?></td>
+				<td><?php echo $site->lastupdate ?></td>
+				<td><?php echo date("Y-m-d H:i:s", strtotime($site->lastbuild)) ?></td>
+				<td><?php echo $site->posts ?></td>
+				<td>
+					<form action="" method="post">
+						<?php wp_nonce_field('pp-ettan-rm-site-' . $key) ?>
+						<input type="hidden" name="key" value="<?php echo $key ?>">
+						<input type="submit" value="Radera">
+					</form>
+				</td>
+			</tr>
+			<?php endforeach ?>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th>Namn</th>
+				<th>Adress</th>
+				<th>Status</th>
+				<th>Senaste hämtning</th>
+				<th>Senast uppdaterad</th>
+				<th>Antal inlägg</th>
+				<th>Radera</th>
+			</tr>
+		</tfoot>
+	</table>
+
+	<?php endif ?>
+
+	<h3>Lägg till ny sajt</h3>
+	<form action="" method="post">
+		<?php wp_nonce_field('pp-ettan-add-site') ?>
+
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th>
+						<label for="pp1-url">Adress</label>
+					</th>
+					<td>
+						<input type="url" name="url" id="pp1-url" placeholder="http://example.com/" required>
+					</td>
+				</tr>
+				<tr>
+					<th>
+						<label for="pp1-name">Namn</label>
+					</th>
+					<td>
+						<input type="text" name="name" id="pp1-name" required>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<p class="submit">
+			<input type="submit" class="button-primary" value="Lägg till">
+		</p>
+	</form>
+
+	<h2>Inställningar</h2>
+	<p>Todo inställningar</p>
+</div>
---
+++ pp-ettan/pp-ettan.php
@@ -0,0 +1,236 @@
+<?php
+/*
+Plugin Name: Piratpartiet startsida
+Plugin URI: http://www.piratpartiet.se
+Description: Hämta inlägg från underbloggar och visa på startsidan
+Version: 1.0
+Author: Rickard Andersson
+Author URI: https://0x539.se
+*/
+
+/**
+ * This is where you add comments for your plugin class
+ * @author Rickard Andersson
+ */
+class PP_ettan {
+
+	private $plugin_name = "pp-ettan";
+
+	/**
+	 * The constructor is executed when the class is instatiated and the plugin gets loaded.
+	 * @return \PP_Ettan
+	 * @since 1.0
+	 */
+	function __construct() {
+		// Add the options menu
+		add_action('admin_menu', array($this,'init_admin_menu'));
+
+		// Filters for loading the rss content instead
+		add_filter('get_comments_number', array($this, 'get_comments_number'));
+		add_filter('the_tags', array($this, 'the_tags'), 10, 4);
+	}
+
+	/**
+	 * Add the options page
+	 * @since 1.0
+	 * @return void
+	 */
+	function init_admin_menu() {
+		add_options_page('Ettan', 'Ettan', 'manage_options', $this->plugin_name, array($this, 'options_page_ettan'));
+	}
+
+	/**
+	 * Attached to the filter 'get_comments_number' and returns the number of comments from RSS
+	 * @return int
+	 * @since 1.0
+	 */
+	function get_comments_number() {
+		global $post;
+		return isset($post->comment_count) ? $post->comment_count : 0;
+	}
+
+	/**
+	 * Attached to the filter 'the_tags' and returns the tags from RSS
+	 * @param $terms
+	 * @param $before
+	 * @param $sep
+	 * @param $after
+	 * @return string
+	 * @since 1.0
+	 */
+	function the_tags($terms, $before, $sep, $after) {
+		global $post;
+		return $before . implode($sep, $post->tags) . $after;
+	}
+
+	/**
+	 * Load posts from the RSS sites
+	 * @since 1.0
+	 * @return void
+	 */
+	function load_posts() {
+
+		$sites = get_option('pp-ettan-sites');
+		$posts = array();
+
+		// Iterate all the sites
+		foreach ( $sites as $key => $site ) {
+
+			$sites[ $key ]->lastupdate = date("Y-m-d H:i:s");
+
+			// Create the URI for the feed
+			$uri = $site->url;
+			$uri .= substr($uri, -1, 1) != '/' ? '/' : '';
+			$uri .= '?feed=rss2';
+
+			// Try to load the RSS feed
+			$feed = fetch_feed($uri);
+
+			// Update the status to "Error" if loading fails
+			if ( is_wp_error($feed) ) {
+				$sites[ $key ]->status    = "Error";
+				$sites[ $key ]->lastbuild = "";
+				$sites[ $key ]->posts     = 0;
+ 			} else {
+				// Fetch the channel and items for easier access
+				$channel = $feed->data['child']['']['rss'][0]['child']['']['channel'][0]['child'][''];
+				$items   = $channel['item'];
+
+				foreach ( $items as $item ) {
+					$post = new stdClass;
+
+					// GUID + ID
+					$guid = $item['child']['']['guid'][0]['data'];
+					preg_match("/\?p=([0-9]+)/", $guid, $matches);
+					$ID = $matches[1];
+
+					// Tags
+					$tags = $item['child']['']['category'];
+					$post->tags = array();
+
+					foreach ( $tags as $tag ) {
+						$post->tags[] = $tag['data'];
+					}
+
+					// General simple attributes
+					$post->ID            = "$key:$ID";
+					$post->title         = $item['child']['']['title'][0]['data'];
+					$post->permalink     = $item['child']['']['link'][0]['data'];
+					$post->excerpt       = $item['child']['']['description'][0]['data'];
+					$post->post_date     = $item['child']['']['pubDate'][0]['data'];
+					$post->comment_count = $item['child']['http://purl.org/rss/1.0/modules/slash/']['comments'][0]['data'];
+					$post->class         = 'class="post-'. $post->ID .' post type-post status-publish format-standard hentry"';
+
+					$posts[] = $post;
+				}
+
+				// Update some status fields for the site
+				$sites[ $key ]->status    = "OK";
+				$sites[ $key ]->posts     = count($items);
+				$sites[ $key ]->lastbuild = $channel['lastBuildDate'][0]['data'];
+			}
+		}
+
+		// Resort the posts array to show the latest posts at the top
+		usort($posts, function($a, $b) {
+			$time_a = strtotime($a->post_date);
+			$time_b = strtotime($b->post_date);
+
+			if ( $time_a == $time_b )
+				return 0;
+
+			// Backwards, sort descending
+			return ( $time_a > $time_b ) ? -1 : 1;
+		});
+
+		update_option('pp-ettan-posts', $posts);
+		update_option('pp-ettan-sites', $sites);
+	}
+
+	/**
+	 * Get current posts from cache
+	 * @since 1.0
+	 * @return mixed|void
+	 */
+	function get_posts() {
+		$posts = get_option('pp-ettan-posts');
+		return $posts ? $posts : array();
+	}
+
+	/**
+	 * Handler function for the options page, handles actions from the form and renders the result
+	 * @since 1.0
+	 * @return void
+	 */
+	function options_page_ettan() {
+
+		// Fetch current sites from options
+		$sites  = get_option('pp-ettan-sites');
+		$errors = array();
+
+		if ( !$sites ) {
+			$sites = array();
+		}
+
+		// If the 'delete' button was used
+		if ( isset($_POST['key']) && is_numeric($_POST['key']) && wp_verify_nonce($_POST['_wpnonce'], 'pp-ettan-rm-site-' . $_POST['key']) ) {
+
+			// Unset the site and update the option
+			unset($sites[ $_POST['key'] ]);
+			update_option('pp-ettan-sites', $sites);
+		}
+
+		// If the add site form was submitted
+		if ( wp_verify_nonce($_POST['_wpnonce'], 'pp-ettan-add-site') ) {
+
+			// Construct the feed URL
+			$uri = $_POST['url'];
+			$uri .= substr($uri, -1, 1) != '/' ? '/' : '';
+			$uri .= "?feed=rss2";
+
+			// Validate the resulting URL
+			if ( !filter_var( $uri, FILTER_VALIDATE_URL ) ) {
+				$errors[] = "Ogiltig adress '" . filter_var( $_POST['url'], FILTER_SANITIZE_URL ) . "'";
+			}
+
+			else {
+
+				// Fetch the feed
+				$feed = fetch_feed( $uri );
+
+				if ( is_wp_error($feed) ) {
+					$errors[] = "Ett fel uppstod när innehåll skulle hämtas: '" . $feed->get_error_message() . "'";
+				} else {
+
+					// Get the generator attribute from the feed
+					$generator = $feed->data['child']['']['rss'][0]['child']['']['channel'][0]['child']['']['generator'][0]['data'];
+
+					// ..and chech that it's WordPress
+					if ( substr($generator, 0, 24) != "http://wordpress.org/?v=" ) {
+						$errors[] = "Adressen verkar inte peka mot en WordPress blogg, generator rapporterar '" . $generator . "'";
+					}
+				}
+			}
+
+			// If everything went fine, create a new object and add it to current sites
+			if ( count($errors) == 0) {
+				$site = new stdClass;
+				$site->url        = $_POST['url'];
+				$site->name       = $_POST['name'];
+				$site->error      = false;
+				$site->lastupdate = false;
+				$site->lastbuild  = false;
+				$site->status     = 'Unknown';
+				$site->posts      = 0;
+
+				$sites[] = $site;
+
+				update_option('pp-ettan-sites', $sites);
+			}
+		}
+
+		require "pages/options_page_ettan.php";
+	}
+}
+
+$ettan = new PP_ettan();
