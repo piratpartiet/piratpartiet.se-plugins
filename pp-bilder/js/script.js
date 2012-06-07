90e4f5c908524acbc69d896a5f81ad6d226689aa
rickard <rickard@montania.se>
2012-06-08 00:31:22 +0200

    Öppnet upp PPBilder JS objektet för global scope Fixat bildväljaren Bytt strategi för att få in vår egna bildväljare från utbytt callback till filter

--- pp-bilder/js/script.js
+++ pp-bilder/js/script.js
@@ -1,35 +1,39 @@
-(function($){
+(function ($) {
 
-  var PPBilder = {
+  window.PPBilder = {
 
-    init: function() {
+    nonce:false,
+
+    init:function (nonce) {
+      this.nonce = nonce;
+
       $(document).on('click', '#pp-bilder img', this.clickHandler);
     },
 
-    clickHandler: function() {
-      var $this    = $(this),
+    clickHandler:function () {
+      var $this = $(this),
         filename = $this.data('filename'),
-        post_id  = $this.data('post-id');
+        image_id = $this.data('post-id');
 
-      if ( !post_id && !filename ) {
+      if (!image_id && !filename) {
         PPBilder.showError();
         return;
       }
 
-      if ( filename ) {
-        var jqxhr = PPBilder.importImage( filename );
+      if (filename) {
+        var jqxhr = PPBilder.importImage(filename);
 
-        jqxhr.done(function(data) {
-          /[0-9]+/.test(data) ?
+        jqxhr.done(function (data) {
+          /[0-9]+/.test(data) && data != 0 ?
             PPBilder.selectImage(data) :
             PPBilder.showError();
         });
       } else {
-        PPBilder.selectImage(post_id);
+        PPBilder.selectImage(image_id);
       }
     },
 
-    importImage: function(filename) {
+    importImage:function (filename) {
 
       //noinspection JSUnresolvedVariable
       return $.post(ajaxurl, {
@@ -38,15 +42,14 @@
       });
     },
 
-    selectImage: function(post_id) {
-      console.log('todo: implement selectImage');
+    selectImage:function (image_id) {
+      window.post_id = parseInt(jQuery("#post_ID").val(), 10);
+      WPSetAsThumbnail(image_id, PPBilder.nonce);
     },
 
-    showError: function() {
+    showError:function () {
       console.log('todo: implement showError');
     }
   };
-
-  PPBilder.init();
 
 }(jQuery));
